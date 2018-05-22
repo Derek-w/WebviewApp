394ea2cb2d1a6a19178dde856a02f22597c87690
Derek-w <haocheng.wang@mobileappscompany.us>
2018-05-21 20:30:27 -0400

    cardInfo validation

--- WebviewApp/ViewController.swift
+++ WebviewApp/ViewController.swift
@@ -7,19 +7,75 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var webView: ProductWebView!
+    
+    let navigationDelegate = NavigationDelegate()
+    let scriptHandler = ScriptMessageHandler()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        scriptHandler.delegate = self
+        webView.configure(nav: navigationDelegate, script: scriptHandler, homeURL: API.Products.home)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController: JavascriptHandlerDelegate {
+    
+    func received(product: Product) {
+        
+        let alertController = UIAlertController(
+            title: "Enter Payment Information",
+            message: "Credit Card Number",
+            preferredStyle: .alert)
+        
+        let cancel = UIAlertAction(
+            title: "Cancel",
+            style: .default,
+            handler: nil)
+        
+        alertController.addTextField(configurationHandler: nil)
+        
+        let purchase = UIAlertAction(
+            title: "Purchase",
+            style: .default, handler: { [weak alertController] (_)in
+                let textField = alertController?.textFields![0]
+                let text = textField?.text ?? ""
+                if self.cardInfoValidation(cardInfo: text){
+                    self.completePurchase(with: product)
+                } else {
+                    alertController?.message = "Invalid CardInfo!"
+                    self.present(alertController!, animated: true, completion: nil)
+                }
+        })
+        
+        alertController.addAction(purchase)
+        alertController.addAction(cancel)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func cardInfoValidation(cardInfo: String) ->Bool{
+        return cardInfo.count == 16
+    }
+    
+    func completePurchase(with product: Product){
+        
+        guard let orderPage = Page.order else { return }
+        
+        webView.loadHTMLString(orderPage
+                .replacingOccurrences(of: "{name}", with: product.name)
+                .replacingOccurrences(of: "{price}", with: product.price)
+                .replacingOccurrences(of: "{image}", with: product.image),
+                               baseURL: nil)
+    }
+}
